54e0542762d82dbb12c4f3bc9bb69f2923237679
Daniel Walters <daniel.walters@genero.com>
2022-10-13 11:37:26 +1100

    upd

--- nvim/lua/plugin-setup/telescope.lua
+++ nvim/lua/plugin-setup/telescope.lua
@@ -1,7 +1,7 @@
 require("telescope").setup({
-  defaults = {
-    file_ignore_patterns = { "node_modules", ".git" }
-  },
+	defaults = {
+		file_ignore_patterns = { "node_modules", ".git/" },
+	},
 	pickers = {
 		find_files = {
 			hidden = true,
