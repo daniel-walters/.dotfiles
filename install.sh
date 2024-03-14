fbfd022fcc85784942cad9e79fdefa1807e27998
danielwalters <waltersdaniel96@gmail.com>
2024-03-14 22:16:46 +1100

    Remove nvim config
    moved to its own repo /nvim-config

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo ln -si ~/.dotfiles/nvim ~/.config/nvim
 sudo ln -si ~/.dotfiles/alacritty ~/.config/alacritty
 sudo ln -si ~/.dotfiles/tmux ~/.config/tmux
 sudo ln -si ~/.dotfiles/zshrc ~/.zshrc
